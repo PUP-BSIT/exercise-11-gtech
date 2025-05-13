e3a2a6fa32dee5240cbd7498b71090ff3ad96bbc
Adriel Joseph Dimayuga <dimayuga.adrieljoseph03@gmail.com>
2025-05-13 23:06:33 +0800

    update TODO

--- main.py
+++ main.py
@@ -5,14 +5,20 @@
 # lucky_calendar_quote_generator/
 # Entry point for generating the lucky number and quote
 # ├── main.py
+
+#TODO (Althea)
 # Lucky Number Generator (uses `random` and `numpy` modules)
 # ├── lucky_number.py
+#TODO (Adriel Joseph)
 # Quote Generator (uses `quote` module to get quotes)
 # ├── quote_generator.py
-# Adds time effects (uses `datetime` module)
+#TODO (Rain)
+# Adds time (uses `datetime` module)
 # ├── generate_time.py
+#TODO (Grace)
 # Fetches the weekday (uses `calendar` and `datetime` modules)
 # ├── weekday.py
+#TODO (Hoshea)
 # Calculates the square root of the lucky number (uses `math` module)
 # └── sqrt_calculator.py
 
