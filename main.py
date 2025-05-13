a2807c4d31419347389bbd9e5b69eb83fbda9b61
Adriel Joseph Dimayuga <dimayuga.adrieljoseph03@gmail.com>
2025-05-13 22:33:34 +0800

    setup repository

---
+++ main.py
@@ -0,0 +1,22 @@
+#TODO (everyone): from each of your module, add atleast 1 function
+
+# System: Lucky Calendar & Quote Generator
+
+# lucky_calendar_quote_generator/
+# Entry point for generating the lucky number and quote
+# ├── main.py
+# Lucky Number Generator (uses `random` and `numpy`)
+# ├── lucky_number.py
+# Quote Generator (uses `requests` to fetch quotes from an API)
+# ├── quote_generator.py
+# Adds time effects (uses `time`)
+# ├── generate_time.py
+# Fetches the weekday (uses `calendar` and `datetime`)
+# ├── weekday.py
+# Calculates the square root of the lucky number (uses `math`)
+# └── sqrt_calculator.py
+
+# Do not forget to enable virtual environment
+# Place the modules inside the packages folder
+# Make sure to use your surname as the name of the module (dimayuga.py)
+# Follow coding guidelines :>
