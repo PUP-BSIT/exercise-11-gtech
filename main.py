7903161d1b34ea649d59394c8130b41b7fd8668b
Adriel Joseph Dimayuga <dimayuga.adrieljoseph03@gmail.com>
2025-05-13 22:37:44 +0800

    modify TODO

--- main.py
+++ main.py
@@ -5,15 +5,15 @@
 # lucky_calendar_quote_generator/
 # Entry point for generating the lucky number and quote
 # ├── main.py
-# Lucky Number Generator (uses `random` and `numpy`)
+# Lucky Number Generator (uses `random` and `numpy` modules)
 # ├── lucky_number.py
-# Quote Generator (uses `requests` to fetch quotes from an API)
+# Quote Generator (uses `quote` module to get quotes)
 # ├── quote_generator.py
-# Adds time effects (uses `time`)
+# Adds time effects (uses `datetime` module)
 # ├── generate_time.py
-# Fetches the weekday (uses `calendar` and `datetime`)
+# Fetches the weekday (uses `calendar` and `datetime` modules)
 # ├── weekday.py
-# Calculates the square root of the lucky number (uses `math`)
+# Calculates the square root of the lucky number (uses `math` module)
 # └── sqrt_calculator.py
 
 # Do not forget to enable virtual environment
